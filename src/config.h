4b02430af5cb69e7959b1884fa37f2b41e0683c1
Sawaguchi Kakeru <144202943+kakerururu@users.noreply.github.com>
2024-11-20 14:23:17 +0900

    LEDの色、ピンの定義

--- src/config.h
+++ src/config.h
@@ -4,5 +4,19 @@
 
 #include <FastLED.h>
 
+// LEDの色定義
+#define LED_SDERROR CRGB(80, 0, 0)
+#define LED_NTPERROR CRGB(80, 0, 80)
+#define LED_NTP CRGB(0, 80, 0)
+#define LED_LOGGING CRGB(0, 0, 80)
+#define LED_RECEIVED CRGB(0, 80, 80)
+#define LED_NONE CRGB(0, 0, 0)
+
+// ピン定義
+#define PIN_BUTTON 0  // 本体ボタンの使用端子（G0）
+#define PIN_OUTPUT 43 // 外部LED
+#define PIN_LED 21    // 本体フルカラーLEDの使用端子（G21）
+#define NUM_LEDS 1    // 本体フルカラーLEDの数
+
 
 #endif  // DEFINITIONS_H
